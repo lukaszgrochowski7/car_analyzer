40412ee1f1038811bfa40568f5cc70c8733d05c0
Lukasz Grochowski <grochowski.lukasz777@gmail.com>
2026-09-25 00:50:31 +0200

    search enginge

--- database.py
+++ database.py
@@ -25,8 +25,47 @@ def get_cars():
     conn.close()
 
     return cars
-
-def search_cars():
-    ...
-
+    
+def find_car(brand=None,
+            min_price=None,
+            max_price=None,
+            min_mileage=None,
+            max_mileage=None,
+            min_year=None,
+            max_year=None,
+            ):
+    parameters = []
+    conditions = [] 
+    if brand is not None:
+        conditions.append("brand = ?")
+        parameters.append(brand)
+    if min_price is not None:
+        conditions.append("price > ?")
+        parameters.append(min_price)
+    if max_price is not None:
+        conditions.append("price < ?")
+        parameters.append(max_price)
+    if min_mileage is not None:
+        conditions.append("mileage > ?")
+        parameters.append(min_mileage)
+    if max_mileage is not None:
+        conditions.append("mileage < ?")
+        parameters.append(max_mileage)
+    if min_year is not None:
+        conditions.append("make_year > ?")
+        parameters.append(min_year)
+    if max_year is not None:
+        conditions.append("make_year < ?")
+        parameters.append(max_year)
+    conn = sqlite3.connect("cars.db")
+    c = conn.cursor()
+    query = "SELECT * FROM cars"
+    if conditions:
+        query += " WHERE " + " AND ".join(conditions)
+    c.execute(query,tuple(parameters))
+    cars = c.fetchall()
+    conn.close()
+    return cars 
+if __name__ == "__main__":
+    pass
 
